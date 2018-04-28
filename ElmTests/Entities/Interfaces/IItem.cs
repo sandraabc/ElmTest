534fea8efb80af1143af3846a51080cacda217c0
Sandra <sandra.abilleira@simlocal.com>
2018-04-28 16:31:09 +0100

    Adding interface.

---
+++ ElmTests/Entities/Interfaces/IItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.Interfaces
+{
+    interface IItem
+    {
+        Product Product { get; set; }
+        int Quantity { get; set; }
+        decimal TotalPrice { get; }
+
+        void AddMoreQuantity();
+        void DeleteQuantitys();
+        
+    }
+}
